37b840fdb3217d3c9e1b234dcf4aa3adb5a6ffcb
swKim <tjsdnrla1234@gmail.com>
2024-11-26 14:38:36 +0900

    dd

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
-<<<<<<< HEAD
 import React from 'react';
 
 import Todolist from './Todolist';
 
-function App() {
-    return <Todolist />;
-=======
-import React from "react";
-
 import Todolist2 from './components/Tdolist2';
 
-function App(){
-    return<Todolist2/>;
->>>>>>> 666be229b8292d14b57d25b0645d34abfdaf7356
-}
 
+function App() {
+    return <Todolist />;
+
+}
 export default App;
